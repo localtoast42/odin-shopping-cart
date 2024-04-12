ef29831868154795c0e2679b91968666bb53022d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-12 14:48:30 -0400

    Add FakeStore API request effect to CardContainer

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import ProductCard from "./ProductCard";
 
 const CardContainer = () => {
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products?limit=5", { mode: "cors" })
+            .then((response) => response.json())
+            .then((response) => console.log(response))
+            .catch((error) => console.error(error));
+    }, []);
 
     return (
         <div>
