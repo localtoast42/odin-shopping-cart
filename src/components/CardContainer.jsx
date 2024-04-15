31fb64a94dcc79a5b54c28fcac7603adfc5bed56
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-15 17:44:46 -0400

    Apply new styling to ProductCard and container

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -13,8 +13,10 @@ const CardContainer = () => {
     }, []);
 
     return (
-        <div>
-            {itemList.map(item => <ProductCard key={item.id} item={item}/>)}
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {itemList.map(item => <ProductCard key={item.id} item={item}/>)}
+            </div>
         </div>
     );
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 const ProductCard = ({ item }) => {
 
     return (
-      <div>
-        <img src={item.image} alt="" height='100px'/>
-        <h3>{item.title}</h3>
-        <div>
-            <h4>{'$' + item.price.toFixed(2)}</h4>
-            <input type="number" />
-            <button>Add</button>
+      <div className="group relative">
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+            <img 
+                src={item.image} 
+                alt="" 
+                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+            />
+        </div>
+        <div className="mt-4 flex justify-between">
+            <div>
+                <h3 className="text-sm text-gray-700">{item.title}</h3>
+            </div>
+            <p className="text-sm font-medium text-gray-900">{'$' + item.price.toFixed(2)}</p>
         </div>
       </div>
     );
