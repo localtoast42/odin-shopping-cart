90bbd18ec3028e3012e49b7a3bce25a72cb8a135
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-16 14:44:34 -0400

    Add Link elements to nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
 
@@ -7,8 +7,8 @@ function App() {
     <>
       <nav className='flex p-6 bg-sky-500'>
         <div className='flex gap-2'>
-          <h2>Home</h2>
-          <h2>Shop</h2>
+          <Link to="">Home</Link>
+          <Link to="shop">Shop</Link>
         </div>
       </nav>
       <Outlet />
