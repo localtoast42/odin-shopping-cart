e8d7c1d492c9fc9635a995cb40fea8fcacb822b5
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-16 12:07:15 -0400

    Add basic nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
 
   return (
     <>
+      <nav className='flex p-6 bg-sky-500'>
+        <div className='flex gap-2'>
+          <h2>Home</h2>
+          <h2>Shop</h2>
+        </div>
+      </nav>
       <CardContainer />
     </>
   )
