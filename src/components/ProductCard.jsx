716447f795deb36472bfa4ac65d36d6efd709d34
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-04-22 11:33:30 -0400

    Add qty increase/decrease functions to buttons

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,6 +6,20 @@ const ProductCard = ({ item }) => {
 
     const [quantity, setQuantity] = useState(0);
 
+    function handleIncrease() {
+        if (quantity < 99) {
+            const newQty = quantity + 1;
+            setQuantity(newQty);
+        }
+    }
+
+    function handleDecrease() {
+        if (quantity > 0) {
+            const newQty = quantity - 1;
+            setQuantity(newQty);
+        }
+    }
+
     return (
         <div className="product-card">
             <img src={item.image} alt="" className="card-image"/>
@@ -14,9 +28,9 @@ const ProductCard = ({ item }) => {
                 <h4 className="product-price">{'$' + item.price.toFixed(2)}</h4>
                 <div>
                     <div className="qty-input">
-                        <button className="qty-btn">-</button>
+                        <button className="qty-btn" onClick={handleDecrease}>-</button>
                         <div className="qty">{quantity}</div>
-                        <button className="qty-btn">+</button>
+                        <button className="qty-btn" onClick={handleIncrease}>+</button>
                     </div>
                     <button className="add-cart-btn">Add to cart</button>
                 </div>
